a3306ccd76b3ddf8f778c02688c6d3ffd148b901
kazumJ05 <oha31@yahoo.co.jp>
2020-01-11 11:53:07 +0900

    Initial Commit

---
+++ TripTimeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripTimeManager
+//
+//  Created by 大原一倫 on 2020/01/11.
+//  Copyright © 2020 oharakazumasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
